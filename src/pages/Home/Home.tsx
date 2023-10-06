6d01d49a400a6983b87983cde46f652b7991000f
thiha129 <71546593+thiha129@users.noreply.github.com>
2023-10-06 16:49:18 +0700

    add two button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,8 @@
 import axios from 'axios'
 import BarChartVersion1 from '../Chart/BarChartVersion1'
 import BarChartVersion2 from '../Chart/BarChartVersion2'
-
 export default function Home() {
-  const a = []
-  a.push('1')
-  const b = a
-
-  console.log('====================================')
-  console.log('b', b)
-  console.log('====================================')
-  const onSubmit = (e: { preventDefault: () => void }) => {
+  const onSubmit1 = (e: { preventDefault: () => void }) => {
     e.preventDefault()
     axios.get('http://detector.com:8071/api/v1/crawl').then((response) => {
       console.log('====================================')
@@ -19,12 +11,20 @@ export default function Home() {
     })
   }
 
+  const onSubmit2 = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    axios.get('http://detector.com:8071/api/v1/crawl').then((response) => {
+      console.log('====================================')
+      console.log('response', response)
+      console.log('====================================')
+    })
+  }
   return (
     <div>
       <div className='container'>
         <div className='grid grid-cols-1 lg:grid-cols-5  lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4 justify-center'>
-            <form className='p-10 rounded bg-white shadow-sm' noValidate onSubmit={onSubmit}>
+            <form className='p-10 rounded bg-white shadow-sm' noValidate onSubmit={onSubmit1}>
               <div className='text-2xl'>Input Text Field</div>
               <div className='mt-8'>
                 <input
@@ -51,9 +51,19 @@ export default function Home() {
                 </button>
               </div>
             </form>
+            <form className='p-10 rounded bg-white shadow-sm' noValidate onSubmit={onSubmit2}>
+              <div className='mt-3'>
+                <button
+                  type='submit'
+                  className='w-full  py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600 flex justify-center items-center'
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
           <div className='lg:col-span-5 lg:col-start-1 justify-center'>
-            <BarChartVersion1 product={b} />
+            <BarChartVersion1 />
             <BarChartVersion2 />
           </div>
         </div>
@@ -61,3 +71,4 @@ export default function Home() {
     </div>
   )
 }
+const a = StyleSheet
